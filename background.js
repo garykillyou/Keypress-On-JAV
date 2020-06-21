bcbd7b039cf88bec78d86fe1a7a511d8730b7b27
Gary Chien <az20151114@gmail.com>
2020-06-21 13:19:58 +0800

    增加功能
    新增要求瀏覽歷史的權限
    新增按下V鍵後，向後臺查詢沒看過的連結，並開啟10個沒看過的連結
    新增按下C鍵後，開啟10個沒看過的連結（要先使用V建新增沒看過的連結）

--- background.js
+++ background.js
@@ -1,24 +1,38 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function () {
-    // Replace all rules ...
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        // With a new rule ...
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                // That fires when a page's URL contains a 'g' ...
-                conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: {
-                            hostEquals: 'javtorrent.re',
-                        },
-                    })
-        ],
-                // And shows the extension's page action.
-                actions: [new chrome.declarativeContent.ShowPageAction()]
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    console.log(sender.tab ?
+      "from a content script:" + sender.tab.url :
+      "from the extension");
+    if (request.hrefList != undefined) {
+      let res = chrome.history.search({ text: request.hrefList[0] }, function (results) {
+        console.log(results);
+        sendResponse({ results });
+      });
+    }
+    else console.log('request.hrefList is undefined');
+  }
+);
+
+chrome.runtime.onConnect.addListener(function (port) {
+  console.assert(port.name == "knockknock");
+  port.onMessage.addListener(async function (msg) {
+    if (msg.hrefList != undefined) {
+      let response = [];
+      let dateMS = Date.now() - (86400000 * 7);
+      for (let href of msg.hrefList) {
+        let tmp = await SearchAsync(href, dateMS);
+        if (tmp.length == 0) response.push(href);
       }
-    ]);
-    });
+      port.postMessage(response);
+    }
+    else console.log('msg.hrefList is undefined');
+  });
 });
+
+function SearchAsync(href, dateMS) {
+  return new Promise((resolve) => {
+    chrome.history.search({ text: href, startTime: dateMS, maxResults: 1 }, function (results) {
+      resolve(results);
+    });
+  });
+}
--- key_event.js
+++ key_event.js
@@ -3,31 +3,44 @@ if (window == top) {
     window.addEventListener('keyup', doKeyPress, false);
 }
 
+window.onload = function () {
+
+};
+
 function doKeyPress(e) {
     console.log(e);
     let tmp;
     if (e.code == 'KeyA') {
-        tmp = $('.pre.pr-1').attr('href');
-        location.assign(tmp);
-        console.log('上一支影片')
+        tmp = document.querySelector('.pre.pr-1')?.getAttribute('href');
+        if (tmp != undefined) {
+            location.assign(tmp);
+            console.log('上一支影片');
+        }
     } else if (e.code == 'KeyD') {
-        tmp = $('.nex.ne-1');
-        if (tmp.length > 0) {
-            location.assign(tmp.attr('href'));
-            console.log('下一支影片')
+        tmp = document.querySelector('.nex.ne-1')?.getAttribute('href');
+        if (tmp != undefined) {
+            location.assign(tmp);
+            console.log('下一支影片');
         }
     } else if (e.code == 'KeyS') {
         copyTitle();
-        let url = $('.single-t a').length > 1 ? $('.single-t a')[0].attributes.href : $('.single-t a').attr('href')
+        let url = document.querySelector('.single-t a')?.getAttribute('href');
         window.open(url);
     } else if (e.code == 'KeyW') {
-        tmp = "http://jtl.re/d/" + $('.j-link').attr('href').split("=")[1] + ".torrent";
+        // tmp = `http://jtl.re/d/${document.querySelector('a.j-link')?.getAttribute('href').split("=")[1]}.torrent`;
+        // tmp = "http://jtl.re/d/" + $('.j-link').attr('href').split("=")[1] + ".torrent";
+        tmp = document.querySelector('a.j-link')?.getAttribute('href');
         console.log(tmp);
-        location.href = tmp;
+        location.assign(tmp);
         console.log('複製標題並下載影片種子檔');
     } else if (e.code == 'KeyX') {
         window.close();
+    } else if (e.code == 'KeyV') {
+        QueryHistory();
+    } else if (e.code == 'KeyC') {
+        OpenWindows();
     }
+
 }
 
 function copyTitle() {
@@ -40,9 +53,27 @@ function copyTitle() {
     $('.s[name="s"]').val('');
 }
 
-function openUnvisited() {
-    let tmp = $('span.base-t');
-    tmp.filter(function (i, e) {
-        return $(e).css('color') == 'rgb(68, 85, 255)';
+var linkList = [];
+
+function QueryHistory() {
+    let aList = document.querySelectorAll('div.base.rank div a:first-child');
+    let hrefList = [];
+    aList.forEach(el => {
+        hrefList.push(el.getAttribute('href'));
     });
+
+    let port = chrome.runtime.connect({ name: "knockknock" });
+    port.postMessage({ hrefList });
+    port.onMessage.addListener(function (response) {
+        console.log(response);
+        linkList = response;
+        OpenWindows();
+    });
+}
+
+function OpenWindows() {
+    console.log(linkList);
+    for (let i = 10; i > 0 && linkList.length > 0; i--) {
+        window.open(linkList.pop());
+    }
 }
